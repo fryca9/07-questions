7aef2c99db8e27617b90c584f91702032c06031e
ErykF07 <frackowiak.eryk@gmail.com>
2023-11-25 20:01:15 +0100

    finished version

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+const toggle = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
+
+toggle.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    questions[index].classList.toggle("show-text");
+  });
+});
